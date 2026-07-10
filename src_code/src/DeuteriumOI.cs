ec76af03a824df42ed53b2ec728efb9a2a38727c
BrokenByteOfCode <Andriy4521@ukr.net>
2026-07-11 00:41:03 +0300

    I no have words

--- src_code/src/DeuteriumOI.cs
+++ src_code/src/DeuteriumOI.cs
@@ -31,56 +31,6 @@ namespace Deuterium
                 new OpTab(this, OptionInterface.Translate("About"))
             };
 
-            string displayName = !string.IsNullOrEmpty(mod.LocalizedName) ? mod.LocalizedName : mod.name;
-            Tabs[0].AddItems(new OpLabel(new Vector2(75f, 540f), new Vector2(450f, 50f), displayName, FLabelAlignment.Center, bigText: true));
-
-            string description = mod.LocalizedDescription;
-            if (!string.IsNullOrEmpty(description))
-            {
-                Tabs[0].AddItems(new OpLabel(new Vector2(75f, 480f), new Vector2(450f, 60f), description, FLabelAlignment.Center)
-                {
-                    autoWrap = true,
-                    color = MenuColorEffect.rgbMediumGrey
-                });
-            }
-
-            Tabs[0].AddItems(new OpLabel(new Vector2(75f, 440f), new Vector2(450f, 30f),
-                $"{OptionInterface.Translate("Code of Mod")}: BrokenByteOfCode", FLabelAlignment.Center)
-            {
-                color = MenuColorEffect.rgbMediumGrey
-            });
-            Tabs[0].AddItems(new OpLabel(new Vector2(75f, 420f), new Vector2(450f, 30f),
-                $"{OptionInterface.Translate("Localization")}: batya_v_khati", FLabelAlignment.Center)
-            {
-                color = MenuColorEffect.rgbMediumGrey
-            });
-
-            Tabs[0].AddItems(new OpLabel(new Vector2(75f, 380f), new Vector2(450f, 30f),
-                OptionInterface.Translate("Languages added by this mod:"), FLabelAlignment.Center));
-
-            float yPos = 360f;
-            LanguageEntry[] enabled = Languages.All.Where(l => l.Enabled).ToArray();
-            if (enabled.Length == 0)
-            {
-                Tabs[0].AddItems(new OpLabel(new Vector2(75f, yPos), new Vector2(450f, 30f),
-                    OptionInterface.Translate("(no languages enabled)"), FLabelAlignment.Center)
-                {
-                    color = MenuColorEffect.rgbMediumGrey
-                });
-            }
-            else
-            {
-                foreach (var lang in enabled)
-                {
-                    Tabs[0].AddItems(new OpLabel(new Vector2(75f, yPos), new Vector2(450f, 30f),
-                        OptionInterface.Translate(lang.DisplayName), FLabelAlignment.Center)
-                    {
-                        color = MenuColorEffect.rgbMediumGrey
-                    });
-                    yPos -= 20f;
-                }
-            }
-
             OpSimpleButton button = new OpSimpleButton(new Vector2(225f, 290f), new Vector2(150f, 30f), OptionInterface.Translate("Press me"))
             {
                 description = OptionInterface.Translate("Press at your own risk.")
